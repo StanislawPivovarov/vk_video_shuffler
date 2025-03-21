61fb7d4b906482db6cab2f7d11ee751b7341a9c4
Стас Пивоваров <stas.povovarov@headmade.pro>
2025-03-21 21:31:47 +0300

    feat: add vk auth

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,15 @@ import * as VKID from '@vkid/sdk';
 function App() {
 
   VKID.Config.init({
-    app: 53306280,
-    redirectUrl: 'localhost:3000',
+    app: 53306336,
+    redirectUrl: 'https://stanislawpivovarov.github.io/vk_video_shuffler/',
   });
 
   VKID.Auth.login();
 
+
   return (
+
     <div className="App">
       <Row justify={'center'}>
         <Col span={10}>
